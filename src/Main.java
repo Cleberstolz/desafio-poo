58452be32591e451356b9bfd24a4db4363b609f0
Cleber Stolz <cleberst_stolz@hotmail.com>
2022-03-05 12:55:07 -0300

    commit devs e bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,8 +20,40 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp java developer descrição");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("conteudos inscritos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("conteudos inscritos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("conteudos concluidos Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+
+        System.out.println("------------------");
+
+        Dev devCleber = new Dev();
+        devCleber.setNome("Cleber");
+        devCleber.inscreverBootcamp(bootcamp);
+        System.out.println("conteudos inscritos Cleber: " + devCleber.getConteudosInscritos());
+        devCleber.progredir();
+        devCleber.progredir();
+        devCleber.progredir();
+        System.out.println("-");
+        System.out.println("conteudos inscritos Cleber: " + devCleber.getConteudosInscritos());
+        System.out.println("conteudos concluidos Cleber: " + devCleber.getConteudosConcluidos());
+        System.out.println("XP: " + devCleber.calcularTotalXp());
     }
 }
